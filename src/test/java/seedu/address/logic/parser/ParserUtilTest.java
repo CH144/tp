546f29d9250021498eaf10d97476ca7ef85e66da
Somebody-somewher <73779218+Somebody-somewher@users.noreply.github.com>
2026-03-06 22:52:18 +0800

    ParserUtilTest.java: Add invalid Length Tag Test

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -26,6 +26,8 @@ public class ParserUtilTest {
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
     private static final String INVALID_TAG = "[friend]";
+    private static final String INVALID_TAG_BY_LENGTH = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
 
     private static final String VALID_NAME = "Rachel Walker";
     private static final String VALID_PHONE = "123456";
@@ -157,6 +159,12 @@ public class ParserUtilTest {
     public void parseTag_invalidValue_throwsParseException() {
         assertThrows(ParseException.class, () -> ParserUtil.parseTag(INVALID_TAG));
     }
+
+    @Test
+    public void parseTag_invalidLength_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseTag(INVALID_TAG_BY_LENGTH));
+    }
+
 
     @Test
     public void parseTag_validValueWithoutWhitespace_returnsTag() throws Exception {
