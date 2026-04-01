fe1e456a7dda9717ce6e59e8fe2ef4d96c466cdb
Xiao Hui <lowxiaohui05@gmail.com>
2026-04-01 12:09:42 +0800

    Fix bug with inconsistent error messages for missing INDEX

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -28,6 +28,7 @@ import seedu.address.model.tag.TagNameComparator;
 public class ParserUtil {
 
     public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
+    public static final String MESSAGE_NO_INDEX = "Index not provided.";
 
     /**
      * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
@@ -36,6 +37,9 @@ public class ParserUtil {
      */
     public static Index parseIndex(String oneBasedIndex) throws ParseException {
         String trimmedIndex = oneBasedIndex.trim();
+        if (trimmedIndex.equals("")) {
+            throw new ParseException(MESSAGE_NO_INDEX);
+        }
         if (!StringUtil.isNonZeroUnsignedInteger(trimmedIndex)) {
             throw new ParseException(MESSAGE_INVALID_INDEX);
         }
